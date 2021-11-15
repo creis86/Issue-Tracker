cc1f0be00ba0e03d0b21dc027c9e9dead5643e3f
creis86 <creis.git@gmail.com>
2021-11-15 18:06:17 -0300

    End of Chapter 5

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const issuesDB = [
         effort: 5,
         created: new Date("2021/11/04"),
         due: undefined,
-        title: "Error in console when clicking Add"
+        title: "Error in console when clicking Add."
     },
     {
         id: 2,
@@ -23,13 +23,13 @@ const issuesDB = [
         effort: 14,
         created: new Date("2021/10/29"),
         due: new Date("2021/11/12"),
-        title: "Missing bottom border on panel"
+        title: "Missing bottom border on panel."
     }
 ];
 
 const GraphQLDate = new GraphQLScalarType({
     name: "GraphQLDate",
-    description: "A Date() type in GraphQL as a scalar",
+    description: "A Date() type in GraphQL as a scalar.",
 
     serialize(value) {
         return value.toISOString();
@@ -80,10 +80,10 @@ function issueAdd(_, { issue }) {
 function issueValidate(issue) {
     const errors = [];
     if(issue.title.length < 3) {
-        errors.push("Field 'title' must be at least 3 characters long");
+        errors.push("Field \"title\" must be at least 3 characters long.");
     }
     if(issue.status === "Assigned" && !issue.owner) {
-        errors.push("Field 'owner' is required when status is 'Assigned'");
+        errors.push("Field \"owner\" is required when status is \"Assigned\".");
     }
     if(errors.length > 0) {
         throw new UserInputError("Invalid input(s)", { errors });
@@ -106,5 +106,5 @@ app.use(express.static("public"));
 server.applyMiddleware({ app, path: "/graphql" });
 
 app.listen(3000, () =>
-    console.log("App started on port 3000")
+    console.log("App started on port 3000.")
 );
